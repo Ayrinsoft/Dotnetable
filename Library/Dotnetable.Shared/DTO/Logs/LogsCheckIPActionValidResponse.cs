0ad0eee282e1fe685939016afef89673b8c8f422
ali youhannaei <ali.youhannaei@outlook.com>
2023-10-06 14:13:35 +0330

    lost files

---
+++ Library/Dotnetable.Shared/DTO/Logs/LogsCheckIPActionValidResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Dotnetable.Shared.DTO.Logs;
+
+public class LogsCheckIPActionValidResponse
+{
+    public int TryCount { get; set; }
+    public bool ValidForNewRequest { get; set; }
+
+    public Public.ErrorExceptionResponse ErrorException { get; set; }
+}
